5856cdf23a61494abf2decc471ce89e7ea1521dc
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-09 02:45:19 +0000

    finished printing sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - input
+ *
+ * Return: null
+ */
+
+int main(void)
+{
+	int Acharacter;
+	char Ainteger;
+	long Along;
+	long long Alonglong;
+	float Afloat;
+
+/*	Acharacter = sizeof(Acharacter);
+ *	Ainteger = sizeof(Ainteger);
+ *	Along = sizeof(Along);
+	Alonglong = sizeof(Alonglong);
+	Afloat = sizeof(Afloat);
+*/
+	printf("Size of a char: %lu byte(s)\n\
+Size of an int: %lu byte(s)\n\
+Size of a long int: %lu byte(s)\n\
+Size of a long long int: %lu byte(s)\n\
+Size of a float: %lu byte(s)\n",
+	       sizeof(Acharacter), sizeof(Ainteger), sizeof(Along),
+	       sizeof(Alonglong), sizeof(Afloat));
+
+	return (0);
+}
