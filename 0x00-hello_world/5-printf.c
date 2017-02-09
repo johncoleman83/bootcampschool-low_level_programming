c819da37a0574df8f20f8297ca7dde1cbfcbe5ec
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-09 02:11:54 +0000

    updating C file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - input
+ * main - void
  *
- * return: null
+ * Return: Null
  */
 
-int main()
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
