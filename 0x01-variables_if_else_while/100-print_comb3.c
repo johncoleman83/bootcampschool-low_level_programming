c8c9d5b0fed438a96d169eb8fbc38e8e667b47be
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-09 22:43:33 +0000

    completed 100, yo!

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -9,6 +7,27 @@
  */
 int main(void)
 {
+	int i;
+	int j;
 
+	i = 0;
+
+	while (i < 90)
+	{
+		if (i > 0 && i % 10 == 0)
+		{
+			j = i / 10;
+			i = i + j + 1;
+		}
+		putchar(i / 10 % 10 + '0');
+		putchar(i % 10 + '0');
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
