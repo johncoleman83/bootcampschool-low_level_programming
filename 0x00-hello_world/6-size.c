4e3eaf75b1334da291bcd4501786cf2f1157c377
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-09 03:12:34 +0000

    updates to 6 format

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,13 @@ int main(void)
 	long long alonglong;
 	float afloat;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of a char: %lu byte(s)\n\
+Size of an int: %lu byte(s)\n\
+Size of a long int: %lu byte(s)\n\
+Size of a long long int: %lu byte(s)\n\
+Size of a float: %lu byte(s)\n",
+	       sizeof(acharacter), sizeof(ainteger), sizeof(along),
+	       sizeof(alonglong), sizeof(afloat));
 
 	return (0);
 }
