3123492fbc9b0a1bc4ff5454c2fc0bcf2507513e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-09 00:13:42 +0000

    first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * program to print message
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
