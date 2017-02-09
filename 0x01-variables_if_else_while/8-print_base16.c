8675abe975d4288bb3ade9e0f903717bb26ef571
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-09 18:40:50 +0000

    adding all file templates to github

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * function_name - main
+ * @parameterx: delete if void
+ *
+ * Description: C programming learning
+ *
+ * Return: 0 no return
+ */
+
+int main(void)
+{
+
+
+
+	return(0);
+}
