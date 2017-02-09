8cf1b18a04e047852287dcc6c7e7ea2ca2fe21e7
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-09 02:46:43 +0000

    updating size checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,12 +14,6 @@ int main(void)
 	long long Alonglong;
 	float Afloat;
 
-/*	Acharacter = sizeof(Acharacter);
- *	Ainteger = sizeof(Ainteger);
- *	Along = sizeof(Along);
-	Alonglong = sizeof(Alonglong);
-	Afloat = sizeof(Afloat);
-*/
 	printf("Size of a char: %lu byte(s)\n\
 Size of an int: %lu byte(s)\n\
 Size of a long int: %lu byte(s)\n\
