06e9fa2ebc012710df5f138f532b5079d72aa748
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-10 03:13:59 +0000

    local pass of 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -9,6 +7,37 @@
  */
 int main(void)
 {
+	int i, j;
 
+	i = 12;
+
+	while (i < 790)
+	{
+		if (i % 10 == 0)
+		{
+			if (i == 100)
+				i = 123;
+			else
+			{
+				i > 120 ? (j = (i / 10) % 10) : (j = i / 10);
+				i += j + 1;
+			}
+		}
+		if (i % 100 == 0)
+		{
+			j = i / 100;
+			i += j * 10 + j + 12;
+		}
+		putchar(i / 100 % 10 + '0');
+		putchar(i / 10 % 10 + '0');
+		putchar(i % 10 + '0');
+		if (i < 789)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
