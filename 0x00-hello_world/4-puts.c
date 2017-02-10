2ff66f0c4c5b26e3090a9899d7358f23b504ef4c
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-10 07:36:42 +0000

    update spaces to tabs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Return: nothing
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
