ac05d9d6b2f6f3f7a5e331811dbd60298de2e68b
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-14 15:36:48 +0000

    adding 2 more problems

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * time_table - prints 9x table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int column, row;
+	int sum;
+	column = 0;
+	while (column < 10)
+	{
+		row = 0;
+		while (row < 10)
+		{
+			sum = column * row;
+			if (row == 0)
+			{
+				_putchar(sum + '0');
+			}
+			else if (sum < 10 )
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(sum + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(sum / 10 + '0');
+				_putchar(sum % 10 + '0');
+			}
+			row++;
+		}
+		_putchar('\n');
+		column++;
+	}
+}
