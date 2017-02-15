60cce772024057c5e1c54155b4448d6fe851e8cf
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-15 03:29:47 +0000

    last fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print fibonacci numbers up to 98 numbers total
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long x, y, z;
+
+	x = 1, y = 2;
+	while (i < 98)
+	{
+		printf("%lu", x);
+		if (i < 97)
+		{
+			printf(", ");
+		}
+		z = y;
+		y = x + y;
+		x = z;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
