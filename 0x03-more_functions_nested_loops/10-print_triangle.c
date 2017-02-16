f111bb169d738d511cb4d11f17da007998778eeb
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-16 18:14:10 +0000

    refactoring 10

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -8,12 +8,11 @@
  */
 void print_triangle(int size)
 {
-	int i, size2 = size;
+	int i, size2;
 
 	if (size < 1)
 		_putchar('\n');
-
-	for (; size2 > 0; size2--)
+	for (size2 = size; size2 > 0; size2--)
 	{
 		for (i = size2; i > 1; i--)
 		{
