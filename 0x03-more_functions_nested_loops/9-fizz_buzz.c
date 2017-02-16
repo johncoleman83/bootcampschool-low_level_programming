425270c4c4233b2e9310cb95f6663462a739aaec
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-16 20:17:33 +0000

    removed extraline from putchar for 9

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,15 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else
-			printf("%d ", i);
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		printf(" ");
 		i++;
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
