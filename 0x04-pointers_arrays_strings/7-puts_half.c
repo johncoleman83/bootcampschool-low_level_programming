a81ed10c22b563ab1b376aa3717d192278f29e4b
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-21 22:46:37 +0000

    modified 7 to have if statement

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,7 @@ void puts_half(char *str)
 	while (str[length] != 0)
 		length++;
 	if (length > 1)
-		for (c = length / 2 + (length % 2 == 0 ? 0 : 1); c <= length; c++)
+		for (c = length / 2 + (length % 2 == 0 ? 0 : 1); c < length; c++)
 			_putchar(str[c]);
 	_putchar('\n');
 }
