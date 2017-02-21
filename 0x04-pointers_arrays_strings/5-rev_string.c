6641e08efb342b5b12d556a1cd30995157244603
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-21 20:11:54 +0000

    update 5

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
 /**
  * rev_string - reverses a string
@@ -9,12 +8,12 @@
  */
 void rev_string(char *s)
 {
-	int length = ARRAY_SIZE(s), let;
-	char copy[ARRAY_SIZE(s) + 1];
+	int length = 0, c;
+	char copy[1000];
 
-	for (let = 0; let <= length; let++)
-		copy[let] = s[let];
-	for (let = 0; length >= 0; length--, let++)
-		s[let] = copy[length];
-	s[let] = 0;
+	for (c = 0; s[c] != 0; c++, length++)
+		copy[c] = s[c];
+
+	for (c = 0; length >= 0; length--, c++)
+		s[c] = copy[length];
 }
