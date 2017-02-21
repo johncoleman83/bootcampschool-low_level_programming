f5c6702a31366cbe42c6f2f508b9d1ef08d21ab6
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-21 23:58:33 +0000

    added 8

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n number of indexes of an array
+ * @a: input array
+ * @n: input num of indexes to print
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int c;
+	char s[] = ", ", last[] = "\n";
+
+	for (c = 0; c < n; c++)
+		printf("%d%s", a[c], c < n - 1 ? s : last);
+}
