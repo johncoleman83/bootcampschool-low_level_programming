a5fec7ff7bb767900c19be13e23d6d6d0a48d143
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-22 07:30:09 +0000

    updated negative conversion

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -8,19 +8,18 @@
  */
 int _atoi(char *s)
 {
-	int result = 0, c = 0, sign = -1;
+	int result = 0, sign = 0, c;
 
-	while (s[c] != '\0')
+	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == '-')
-			sign *= -1;
+			sign++;
 		if (s[c] > 47 && s[c] < 58)
 		{
 			while (s[c] > 47 && s[c] < 58)
 				result = result * 10 - (s[c++] - 48);
 			break;
 		}
-		c++;
 	}
-	return (result * sign);
+	return (result *= sign % 2 == 0 ? -1 : 1);
 }
