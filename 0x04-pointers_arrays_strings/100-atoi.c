b6ae2289ffca0ac7fce13c3cee00668a8b859dc2
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-22 07:02:39 +0000

    updating 100

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,12 @@
  */
 int _atoi(char *s)
 {
-	int result = 0, nth = 1, c = 0, digits = 0, start = 0, sign = 0;
+	int result = 0, nth = 1, c = 0, digits = 0, start = 0, sign = 1;
 
 	while (s[c] != '\0')
 	{
-		if (s[c] == '-' || s[c] == '+')
-			s[c] == '+' ? sign++ : sign--;
+		if (s[c] == '-')
+			sign *= -1;
 		if (s[c] > 47 && s[c] < 58)
 		{
 			start += c;
@@ -30,7 +30,7 @@ int _atoi(char *s)
 			nth *= 10;
 		while (start <= c)
 		{
-			if (sign >= 0)
+			if (sign == 1)
 				result += (s[start] - 48) * nth;
 			else
 				result -= (s[start] - 48) * nth;
