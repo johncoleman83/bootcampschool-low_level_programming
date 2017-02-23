8b18fb24fac5150f17653a245a1e247abcb97b37
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-23 20:25:29 +0000

    updating 2

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j, i = 0;
-
-	while (dest[i] != '\0')
-		i++;
-	for (j = 0; j < n && dest[j] != '\0' && src[j] != '\0'; j++)
+	int j;
+	
+	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[j] = src[j];
-	if (j < n)
-		for (i = 0; j <= i; j++)
-			dest[i] = '\0';
+	for (; j < n; j++)
+		dest[j] = '\0';
 	return (dest);
 }
