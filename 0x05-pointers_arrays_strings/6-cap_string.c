cc1c0659867b3b62a50b7f9e291259520d43600c
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-23 23:04:25 +0000

    completed 6 capitalize words of a string

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * cap_string - converts string words to be capitalized
+ * @s: input string to have all words capitalized
+ *
+ * Return: string after conversion to Capitalize words
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	if (s[0] > 96 && s[0] < 123)
+		s[0] -= 32;
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+			if (s[i + 1] > 96 && s[i + 1] < 123)
+				s[i + 1] -= 32;
+	return (s);
+}
