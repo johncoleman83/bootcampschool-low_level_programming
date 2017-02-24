4d0c9bfb18c12b9aca1b36ac75e6fae1aba64d11
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-24 01:10:57 +0000

    8 rot

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * rot13 - translates string to ROT13
+ * @s: input string to be translated
+ *
+ * Return: string after conversion of ROT13
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot_it[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < 52; j++)
+			if (s[i] == alphabet[j])
+				s[i] = rot_it[j];
+	return (s);
+}
