274641696fb715878479b6cbd94a19ec5c54bd5a
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-24 01:59:45 +0000

    refactored 8

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -8,18 +8,12 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char alphabet[] = "abcdefghijklmABCDEFGHIJKLM";
-	char rot_it[] =   "nopqrstuvwxyzNOPQRSTUVWXYZ";
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot_it[] =   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; alphabet[j] != '\0'; j++)
-		{
+		for (j = 0; j < 52; j++)
 			if (s[i] == alphabet[j])
-				s[i] = rot_it[j];
-			else if (s[i] == rot_it[j])
-				s[i] = alphabet[j];
-		}
-	}
+				s[i] = rot_it[j], j = 52;
 	return (s);
 }
