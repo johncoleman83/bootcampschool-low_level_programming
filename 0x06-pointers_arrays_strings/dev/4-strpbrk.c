5be06ede692f5bc6de1b55769da541b7deb17681
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-27 23:23:16 +0000

    completed 4

--- 0x06-pointers_arrays_strings/dev/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/dev/4-strpbrk.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 /**
- * _strpbrk - input
- * @s: arg
- * @accept: arg
- * Return: pointer to s string
+ * _strpbrk - finds first matching char in string
+ * @s: input string to search for matching char
+ * @accept: characters that could be matched
+ * Return: pointer to matching char
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i;
+	unsigned int i, j;
 
-	while (i < n)
-	{
-		*(s + i) = b;
-		i++;
-	}
-	return (s);
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+				goto exit;
+exit: return (s + i);
 }
