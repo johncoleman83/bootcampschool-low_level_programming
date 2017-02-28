e3d48b1218861639ab3e4b548d8027fefe22d74f
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-28 23:29:34 +0000

    refactored loop

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
 			haystack++, needle++, k++;
 		if (*needle == '\0')
-			return (haystack);
+			return (haystack - k);
 		haystack -= (k - 1), needle -= k;
 	}
 	return ('\0');
