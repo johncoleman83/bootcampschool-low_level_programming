6ee449ecf2424481d382f5489563f41994c3539c
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-28 02:06:21 +0000

    change loop

--- 0x06-pointers_arrays_strings/dev/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/dev/8-print_diagsums.c
@@ -7,12 +7,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	unsigned int i;
+	int j = 1;
 
-	while (i < n)
+	while (a[0][j - 1] != 0)
 	{
-		*(s + i) = b;
-		i++;
+		_putchar(a[0][j - 1]);
+		if (j > 1 && j % 8 == 0)
+			_putchar('\n');
+		j++;
 	}
-	return (s);
 }
