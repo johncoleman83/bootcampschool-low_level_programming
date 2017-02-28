aa01ef26173d6c76a382ec904c8ad89938317316
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-28 22:24:16 +0000

    refactored 8

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -10,9 +10,7 @@ void print_diagsums(int *a, int size)
 {
 	int i, j, sum1 = 0, sum2 = 0;
 
-	for (i = 0, j = 0; i < size; i++, j += size)
-		sum1 += a[j + i];
-	for (i = 0, j = size; i < size; i++, j += size)
-		sum2 += a[j - 1 - i];
+	for (j = 0, i = size - 1; j < (size * size); j += size + 1, i += size - 1)
+		sum1 += a[j], sum2 += a[i];
 	printf("%d, %d\n", sum1, sum2);
 }
