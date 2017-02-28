062f19173d05bbf850e7a0a1037b34c73627c8e1
johncoleman83 <lcsw@davidjohncoleman.com>
2017-02-28 23:25:31 +0000

    finally completed all checks

--- 0x06-pointers_arrays_strings/dev/5-strstr.c
+++ 0x06-pointers_arrays_strings/dev/5-strstr.c
@@ -7,14 +7,16 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int k;
 
 	while (*haystack != '\0')
 	{
+		k = 0;
 		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
-			haystack++, needle++;
+			haystack++, needle++, k++;
 		if (*needle == '\0')
 			return (haystack);
-		haystack++;
+		haystack -= (k - 1), needle -= k;
 	}
 	return ('\0');
 }
---
+++ 0x06-pointers_arrays_strings/dev/main_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char *_strstr(char *, char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s = "First, solve the problem. Then, write the code.";
+	char *f = "";
+	char *t;
+
+	t = _strstr(s, f);
+	printf("%s\n", (t == NULL ? "nil" : t));
+	return (0);
+}
