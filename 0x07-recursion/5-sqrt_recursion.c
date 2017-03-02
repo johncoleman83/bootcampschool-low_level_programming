81d7605d89c4ebfbbd10344b0db951d75c98461f
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-02 01:48:32 +0000

    find square root

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,12 +1,26 @@
 #include "holberton.h"
 /**
+ * check_divisor - find square root
+ * @a: main number
+ * @b: divisor to check if its a square root
+ * Return: natural square root or -1
+ */
+int check_divisor(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	if (b * b > a)
+		return (-1);
+	return (check_divisor(a, b + 1));
+}
+/**
  * _sqrt_recursion - input
  * @n: number to check for natural square root
  * Return: natural square root or -1
  */
 int _sqrt_recursion(int n)
 {
-	if (n % n != 0 || n < 0)
+	if (n > 1 && n < 4)
 		return (-1);
-
+	return (check_divisor(n, 1));
 }
