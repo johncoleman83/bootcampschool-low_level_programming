92979c1befce3dacdc26024e702a05e31e312bb7
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-02 01:05:11 +0000

    completed 7

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,15 +1,38 @@
 #include "holberton.h"
 /**
- * is_palindrome - input
+ * string_length - finds string length
+ * @st: input string
+ * Return: integer length
+ */
+int string_length(char *st)
+{
+	if (*st == '\0')
+		return (0);
+	return (string_length(++st) + 1);
+}
+/**
+ * check_letters - check if numbers are equal
+ * @str: input string
+ * @i: beginning index
+ * @j: ending index
+ * Return: boolean
+ */
+int check_letters(char *str, int i, int j)
+{
+	if ((j - i) < 2)
+		return (1);
+	if (str[i] != str[j])
+		return (0);
+	return (check_letters(str, ++i, --j));
+}
+/**
+ * is_palindrome - checks if string is palindrom
  * @s: input string
- * Return: void
+ * Return: boolean
  */
 int is_palindrome(char *s)
 {
-	int i = 0;
+	int len = string_length(s);
 
-	while (i < n)
-	{
-	}
-	return ();
+	return (check_letters(s, 0, --len));
 }
