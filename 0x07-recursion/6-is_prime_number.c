739cab0fd87c52734fb4b6422b0b320514d37019
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-02 00:28:55 +0000

    completed 6

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,10 +1,20 @@
 #include "holberton.h"
 /**
  * is_prime_number - input
- * @s: input string
+ * @n: number to check
+ * @y: divisor
  * Return: void
  */
+int is_prime(int a, int b)
+{
+	if (a < 2)
+		return (0);
+	if (a == b)
+		return (1);
+	return (a % b == 0 ? 0 : (is_prime(a, b + 1)));
+}
+
 int is_prime_number(int n)
 {
-
+	return (is_prime(n, 2));
 }
