7fc32635d9776ca73a135b6d559518568a0d4d8c
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-03 06:13:16 +0000

    refactored

--- 0x07-recursion/dev/100-wildcmp.c
+++ 0x07-recursion/dev/100-wildcmp.c
@@ -1,25 +1,25 @@
 #include "holberton.h"
 /**
- * move_pointer - move pointer to 'c' character in 'd' direction
+ * move_p - move pointer to 'c' character in 'd' direction
  * @s1: check point to stop in rare circumstances
  * @s2: input string
  * @c: character to match or with '*' to find first non-'*'
  * @d: direction forward or backwards: 1 or -1
  * Return: pointer to match
  */
-char *move_pointer(char *s1, char *s2, char c, int d)
+char *move_p(char *s1, char *s2, char c, int d)
 {
 	if (c == '*')
 	{
 		if (*s2 != '*')
 			return (s2);
-		return (move_pointer(s1, s2 + 1, c, d));
+		return (move_p(s1, s2 + 1, c, d));
 	}
 	if (s2 == s1)
 		return (s2);
 	if (*s2 == c)
 		return (s2);
-	return (move_pointer(s1, (d == 1 ? s2 + 1 : s2 - 1), c, (d == 1 ? 1 : -1)));
+	return (move_p(s1, (d == 1 ? s2 + 1 : s2 - 1), c, (d == 1 ? 1 : -1)));
 }
 /**
  * wildcmp - checks is strings could be identical considering * wildcard
@@ -29,20 +29,16 @@ char *move_pointer(char *s1, char *s2, char c, int d)
  */
 int wildcmp(char *s1, char *s2)
 {
-	char *an_address = s1;
-
-	an_address = move_pointer(s2, an_address, '\0', 1);
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s2 == '*')
 	{
-		s2 = move_pointer(s1, s2, '*', 1);
+		s2 = move_p(s1, s2, '*', 1);
 		if (*s2 == '\0')
 			return (1);
 		if (*s1 == '\0')
 			return (0);
-		an_address = move_pointer(s1, an_address, *s2, -1);
-		if (wildcmp(an_address, s2))
+		if (wildcmp(move_p(s1, move_p(s2, s1, '\0', 1), *s2, -1), s2))
 			return (1);
 	}
 	if (*s1 != *s2)
