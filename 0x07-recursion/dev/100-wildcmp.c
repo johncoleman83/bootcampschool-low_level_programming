08964b83f047907deaeebd0c866612ca36c1b18e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-03 00:05:35 +0000

    refactored and accounted for copies of string within first string

--- 0x07-recursion/dev/100-wildcmp.c
+++ 0x07-recursion/dev/100-wildcmp.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * move_pointer - move pointer to character in 'd' direction
  * @s: input string
@@ -33,36 +34,25 @@ char *parse_star(char *s)
  */
 int wildcmp(char *s1, char *s2)
 {
-	int star = 0;
 	char post_l;
-	char *an_address = s1, *post_i1, *post_i2;
+	char *an_address = s1;
 
 	an_address = move_pointer(an_address, '\0', 1);
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(++s1, ++s2));
 	if (*s2 == '*')
 	{
 		s2 = parse_star(s2);
 		if (*s2 == '\0')
 			return (1);
-		post_l = *s2, post_i1 = s1, post_i2 = s2;
-		s1 = move_pointer(s1, post_l, 1);
 		if (*s1 == '\0')
 			return (0);
-		star = 1;
+		post_l = *s2;
+		an_address = move_pointer(an_address, post_l, -1);
+		if (wildcmp(an_address, s2) == 1)
+			return (1);
 	}
 	if (*s1 != *s2)
-	{
-		if (star == 0)
-			return (0);
-		star = 0;
-		an_address = move_pointer(an_address, post_l, -1);
-		if (post_i1 == an_address)
-			return (0);
-		s1 = an_address;
-		s2 = post_i2;
-	}
+		return (0);
 	return (wildcmp(++s1, ++s2));
 }
