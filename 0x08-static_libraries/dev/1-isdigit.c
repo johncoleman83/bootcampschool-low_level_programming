28c2e01de4e7267c5a0b8f357d590ec70364e706
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-03 20:50:28 +0000

    initialized directory 0x08-static_libraries

---
+++ 0x08-static_libraries/dev/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks if input's ascii value is digit 0-9
+ * @c: input variable to check ascii value
+ *
+ * Return: 1 if digit, 0 if not
+ */
+int _isdigit(int c)
+{
+	return (c > 47 && c < 58 ? 1 : 0);
+}
---
+++ 0x08-static_libraries/dev/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _puts - prints string from pointer to string
+ * @str: string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
