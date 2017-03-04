19a1c3b3c37324d46fa97a0f30dc719dee80ec7d
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-04 07:06:56 +0000

    find minimum number of coins to add to input value of cents

---
+++ 0x09-argc_argv/dev/100-change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * @a: number of args
+ * @b: pointer to args
+ * Return: 1 for error, 0 not
+ */
+int main(int a, char **b)
+{
+	if (a != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int sum = atoi(*(b + 1)), coins = 0;
+
+	while (sum >= 25)
+		sum -= 25, coins++;
+	while (sum >= 10)
+		sum -= 10, coins++;
+	while (sum >= 5)
+		sum -= 5, coins++;
+	while (sum >= 2)
+		sum -= 2, coins++;
+	while (sum >= 1)
+		sum--, coins++;
+	printf("%d\n", coins);
+	return (0);
+}
