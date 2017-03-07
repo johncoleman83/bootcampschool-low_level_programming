4961a692a7acaaa8497b109cf06aff2cd8434f31
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-07 23:33:37 +0000

    added if statements to account for null strings

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -10,14 +10,18 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *pstr;
-	unsigned int i = 0, j = 0, k = 0, len;
+	unsigned int i = 0, j = 0, k = 0;
+
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
 
 	while (s1[i] != 0)
 		i++;
 	while (s2[j] != 0)
 		j++;
-	len = j + i;
-	pstr = malloc(sizeof(char) * len);
+	pstr = malloc(sizeof(char) * (i + j));
 	for (i = 0; s1[i] != 0; i++)
 		pstr[i] = s1[i];
 	for (j = i, i = 0; s2[k] != 0; j++, k++)
