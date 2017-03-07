713afdf74a22c2084543f11cab99725de0324a4b
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-07 23:43:16 +0000

    added stringlengths

--- 0x0A-malloc_free/dev/2-str_concat.c
+++ 0x0A-malloc_free/dev/2-str_concat.c
@@ -10,22 +10,22 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *pstr;
-	unsigned int i = 0, j = 0, k = 0;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
 	if (s1 == 0)
 		s1 = "";
 	if (s2 == 0)
 		s2 = "";
 
-	while (s1[i] != 0)
-		i++;
-	while (s2[j] != 0)
-		j++;
-	pstr = malloc(sizeof(char) * (i + j));
-	for (i = 0; s1[i] != 0; i++)
+	while (s1[len1] != 0)
+		len1++;
+	while (s2[len2] != 0)
+		len2++;
+	len2++;
+	pstr = malloc(sizeof(char) * (len1 + len2));
+	for (i = 0; i < len1; i++)
 		pstr[i] = s1[i];
-	for (j = i, i = 0; s2[k] != 0; j++, k++)
-		pstr[j] = s2[k];
-	pstr[j] = 0;
+	for (j = 0; j < len2; i++, j++)
+		pstr[i] = s2[j];
 	return (pstr);
 }
