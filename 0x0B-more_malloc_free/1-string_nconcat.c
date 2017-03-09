cc2305174bf93f3904e5fa9c47c0be0783daef50
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-09 20:52:33 +0000

    added back size of char to malloc function

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	if (n >= len2)
 		n = len2;
-	new_str = malloc(len1 + n + 1);
+	new_str = malloc(sizeof(char) * (len1 + n + 1));
 	if (new_str == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
