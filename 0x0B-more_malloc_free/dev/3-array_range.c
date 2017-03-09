972931eec760befcadfb98d7aa82f0bbcaf64b79
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-09 21:27:41 +0000

    create array of range of ints

--- 0x0B-more_malloc_free/dev/3-array_range.c
+++ 0x0B-more_malloc_free/dev/3-array_range.c
@@ -7,5 +7,22 @@
  */
 int *array_range(int min, int max)
 {
-	return ();
+	int *p, i;
+
+	if (min > max)
+		return (NULL);
+	if (max - min == 0)
+	{
+		p = malloc(sizeof(int));
+		p[0] = min;
+	}
+	else
+	{
+		p = malloc(sizeof(int) * (max - min + 1));
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; min <= max; i++, min++)
+			p[i] = min;
+	}
+	return (p);
 }
