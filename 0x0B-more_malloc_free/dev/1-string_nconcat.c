cb72dc21b4d74e7f120234d8d13c10cdc5dc001a
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-09 20:26:40 +0000

    malloc mem

--- 0x0B-more_malloc_free/dev/1-string_nconcat.c
+++ 0x0B-more_malloc_free/dev/1-string_nconcat.c
@@ -8,5 +8,25 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	return ();
+	int i, j, len1 = 0, len2 = 0;
+	char *str_conct
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (*(s1 + len1) != '\0')
+		len1++;
+	while (*(s2 + len2) != '\0')
+		len2++;
+
+	if (len1 == 0 && len2 == 0)
+		return (NULL);
+	str_conct = malloc(sizeof(char) * (len1 + (len2 < n ? len2 : n) + 1));
+	for (i = 0; i < len1; i++)
+		str_conct[i] = s1[i];
+	for (j = 0; j < n && j < len2; i++, j++)
+		str_conct[i] = s2[j];
+	return (str_conct);
 }
