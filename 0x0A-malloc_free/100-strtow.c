9b28cbfcd85c1e5c5a1d1a74301a30055988ab69
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-09 00:31:57 +0000

    accounted for beginning and ending spaces

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -10,53 +10,57 @@ char **strtow(char *str)
 {
 	char **newstr;
 	int words = 0, i = 0, j = 0, word_len = 1;
-
 	if (str == 0)
 		return (NULL);
 	while (str[i] != 0)
 	{
-		if (str[i] == ' ' && i != 0)
+		if (str[i] == ' ' && str[i + 1] != 0)
 			words++;
 		i++;
 	}
-	if (i > 0 && str[i] != ' ')
-		words++;
-	else
+	if (i < 1)
 		return (NULL);
+	words++;
 	newstr = malloc((words + 1) * sizeof(char *));
 	if (newstr == 0)
-	{
-		free(newstr);
 		return (NULL);
-	}
 	i = 0;
 	while (str[i] != 0)
 	{
-		if (str[i] == ' ')
+		if (str[i] == ' ' && i != 0)
 		{
 			newstr[j] = malloc(sizeof(char *) * word_len);
 			if (newstr[j] == 0)
 				return (NULL);
 			j++, word_len = 0;
 		}
+		if (str[i] == ' ' && i == 0)
+			word_len--;
 		word_len++, i++;
 	}
-	newstr[j] = malloc(sizeof(char) * (word_len));
-	if (newstr[j] == 0)
-		return (NULL);
+	if (str[i - 1] != ' ')
+	{
+		newstr[j] = malloc(sizeof(char) * (word_len));
+		if (newstr[j] == 0)
+			return (NULL);
+	}
 	i = 0, word_len = 0, j = 0;
 	while (str[i] != 0)
 	{
-		if (str[i] == ' ')
+		if (str[i] == ' ' && i != 0)
 		{
 			newstr[j][word_len] = 0;
-			j++, word_len = -1;
+			if (str[i + 1] != 0)
+				j++, word_len = -1;
 		}
 		else
 			newstr[j][word_len] = str[i];
+		if (str[i] == ' ' && i == 0)
+			word_len--;
 		word_len++, i++;
 	}
-	newstr[j][word_len] = 0;
+	if (str[i - 1] != ' ')
+		newstr[j][word_len] = 0;
 	newstr[++j] = NULL;
 	return (newstr);
 }
--- 0x0A-malloc_free/dev/100-strtow.c
+++ 0x0A-malloc_free/dev/100-strtow.c
@@ -9,19 +9,15 @@
 char **strtow(char *str)
 {
 	char **newstr;
-	int words = 0, i = 0, j = 0, word_len = 1, spaces = 0;
-	printf("here\n");
+	int words = 0, i = 0, j = 0, word_len = 1;
 	if (str == 0)
 		return (NULL);
 	while (str[i] != 0)
 	{
-		if (str[i] == ' ')
-			spaces++;
 		if (str[i] == ' ' && str[i + 1] != 0)
 			words++;
 		i++;
 	}
-	printf("here2 i= %d, words= %d\n", i, words);
 	if (i < 1)
 		return (NULL);
 	words++;
@@ -42,32 +38,29 @@ char **strtow(char *str)
 			word_len--;
 		word_len++, i++;
 	}
-	printf("now here 3\n");
 	if (str[i - 1] != ' ')
 	{
 		newstr[j] = malloc(sizeof(char) * (word_len));
 		if (newstr[j] == 0)
 			return (NULL);
 	}
-	printf("here 4\n");
 	i = 0, word_len = 0, j = 0;
 	while (str[i] != 0)
 	{
 		if (str[i] == ' ' && i != 0)
 		{
 			newstr[j][word_len] = 0;
-			j++, word_len = -1;
+			if (str[i + 1] != 0)
+				j++, word_len = -1;
 		}
 		else
-			if (str[i] != ' ')
-				newstr[j][word_len] = str[i];
+			newstr[j][word_len] = str[i];
 		if (str[i] == ' ' && i == 0)
 			word_len--;
 		word_len++, i++;
 	}
-	if (str[i - 1] == ' ')
-		word_len--;
-	newstr[j][word_len] = 0;
-	newstr[j++] = NULL;
+	if (str[i - 1] != ' ')
+		newstr[j][word_len] = 0;
+	newstr[++j] = NULL;
 	return (newstr);
 }
