afd905d73539f6f61da67ddfd1c33bc1f9766977
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-10 01:33:08 +0000

    updated description

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -4,7 +4,7 @@
  * @ptr: pointer to reallocate memory
  * @old_size: size in bytes of allocated memory
  * @new_size: newsize of memory block in bytes
- * Return: void
+ * Return: void pointer to new allocation of memory
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
