c332e66d5588e33bba095b73b5fe677eda1d7b35
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-10 19:13:55 +0000

    updated to account for int_max and allocated memory with 0 value

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <limits.h>
 /**
  * _calloc - allocates memory for an array using malloc
  * @nmemb: elements of memory needed
@@ -7,12 +8,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	unsigned int i;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (size >= UINT_MAX / nmemb || nmemb >= UINT_MAX / size)
 		return (NULL);
 	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
-	return (p);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+	return ((void *)p);
 }
