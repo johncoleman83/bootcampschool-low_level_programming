5ea750023890b5cf3dcc5b651ffee112e3e1f8b4
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-11 00:44:18 +0000

    accounted for input of 0

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -13,26 +13,26 @@ int str_len(char *str)
 	return (len / 2);
 }
 /**
- * str_to_arr - converts array of chars to array of ints
- * @str: input pointer to string
- * @len: length of string
- * @arr: input pointer to arr
- * Return: 1 succes, 0 failure
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: elements of memory needed
+ * @size: size in bytes of each element
+ * Return: pointer to the allocated memory
  */
-int str_to_arr(char *str, int len, int *arr)
+void *_calloc(unsigned int bytes, unsigned int size)
 {
-	int i;
+	unsigned int i;
+	char *p;
 
-	for (i = 0; i < len; i++)
-	{
-		if (str[i] < 48 || str[i] > 57)
-		{
-			printf("Error\n");
-			return (0);
-		}
-		arr[i] = (str[i] - 48);
-	}
-	return (1);
+	if (bytes == 0 || size == 0)
+		return (NULL);
+	if (size >= UINT_MAX / bytes || bytes >= UINT_MAX / size)
+		return (NULL);
+	p = malloc(size * bytes);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < bytes * size; i++)
+		p[i] = 0;
+	return ((void *)p);
 }
 /**
  * multiply - multiplies 2 #'s, prints result, must be 2 #'s
@@ -41,18 +41,28 @@ int str_to_arr(char *str, int len, int *arr)
  * @big: bigger factor
  * @len_b: length of big factor
  * @mul_result: pointer to result
- * Return: void
+ * Return: 0 fail, 1 success
  */
-void multiply(int *small, int len_s, int *big, int len_b, int **mul_result)
+int multiply(char *small, int len_s, char *big, int len_b, int **mul_result)
 {
 	int i = 0, s = len_s - 1, b, product, carry = 0, nums = 0, digits;
 
 	while (i < len_s)
 	{
 		b = len_b - 1, digits = (len_b + len_s - i - 1);
+		if (small[s] < '0' || small[s] > '9')
+		{
+			printf("Error\n");
+			return (0);
+		}
 		while (b >= 0)
 		{
-			product = small[s] * big[b];
+			if (big[b] < '0' || big[b] > '9')
+			{
+				printf("Error\n");
+				return (0);
+			}
+			product = (small[s] - '0') * (big[b] - '0');
 			product += carry;
 			mul_result[nums][digits] += product % 10;
 			digits--, b--, carry = product / 10;
@@ -60,19 +70,20 @@ void multiply(int *small, int len_s, int *big, int len_b, int **mul_result)
 		mul_result[nums][digits] += carry;
 		nums++, i++, s--;
 	}
+	return (1);
 }
 /**
- * add - adds array of strings together
+ * add_print - adds array of strings together and prints result
  * @mul_result: pointer to 2D int array with numbers to add
  * @nums: rows of array
  * @len_r: length of result array
  * Return: void
  */
-void add(int **mul_result, int nums, int len_r)
+void add_print(int **mul_result, int nums, int len_r)
 {
 	int i = len_r - 1, j, k = len_r, sum, carry = 0, *sum_result;
 
-	sum_result = malloc(sizeof(int) * len_r);
+	sum_result = _calloc(sizeof(int), len_r);
 	len_r--;
 	while (i >= 0)
 	{
@@ -86,7 +97,11 @@ void add(int **mul_result, int nums, int len_r)
 		carry = sum / 10, i--, len_r--;
 	}
 	sum_result[len_r] = carry;
-	i = sum_result[0] == 0 ? 1 : 0;
+	i = 0;
+	while (sum_result[i] == 0 && i < k)
+		i++;
+	if (i == k)
+		_putchar('0');
 	while (i < k)
 		_putchar(sum_result[i++] + '0');
 	_putchar('\n');
@@ -99,41 +114,29 @@ void add(int **mul_result, int nums, int len_r)
  */
 int main(int argc, char **argv)
 {
-	int len_s, len_b, temp, len_r, i, j;
-	int **mul_result, *small, *big;
+	int len_s, len_b, temp, len_r, i;
+	int **mul_result;
+	char *small, *big;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	len_s = str_len(argv[1]);
-	len_b = str_len(argv[2]);
+	len_s = str_len(argv[1]), len_b = str_len(argv[2]);
 	len_r = len_s + len_b;
 	if (len_s < len_b)
-	{
-		small = malloc(sizeof(int) * len_s);
-		big = malloc(sizeof(int) * len_b);
-		if (!str_to_arr(argv[1], len_s, small)
-			|| !str_to_arr(argv[2], len_b, big))
-			exit(98);
-	}
+		small = argv[1], big = argv[2];
 	else
 	{
-		small = malloc(sizeof(int) * len_s);
-		big = malloc(sizeof(int) * len_b);
-		if (!str_to_arr(argv[2], len_b, small)
-			|| !str_to_arr(argv[1], len_s, big))
-			exit(98);
+		small = argv[2], big = argv[1];
 		temp = len_b, len_b = len_s, len_s = temp;
 	}
-	mul_result = malloc(sizeof(int) * (len_s));
+	mul_result = _calloc(sizeof(int), len_s);
 	for (i = 0; i < len_s; i++)
-		mul_result[i] = malloc(sizeof(int) * (len_r));
-	for (i = 0; i < len_s; i++)
-		for (j = 0; j < len_r; j++)
-			mul_result[i][j] = 0;
-	multiply(small, len_s, big, len_b, mul_result);
-	add(mul_result, len_s, len_r);
+		mul_result[i] = _calloc(sizeof(int), len_r);
+	if (!multiply(small, len_s, big, len_b, mul_result))
+		exit(98);
+	add_print(mul_result, len_s, len_r);
 	return (0);
 }
--- 0x0B-more_malloc_free/dev/holberton.h
+++ 0x0B-more_malloc_free/dev/holberton.h
@@ -2,6 +2,7 @@
 #define HEADERFILE_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
