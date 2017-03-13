f002f924eb853c88c270d8691e0d55b1cf288597
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-13 20:51:32 +0000

    removed malloc

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - creates a new dog type from struct dog
  * @d: new dog structure name
@@ -9,13 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(sturct dog));
-	if (d == NULL)
-		;
-	else
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
