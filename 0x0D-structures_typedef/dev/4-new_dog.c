fb49f36c7dc160144dacae1653e046e2dfe02a6e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-13 21:59:55 +0000

    updated to account for freeing all elements of struct

--- 0x0D-structures_typedef/dev/4-new_dog.c
+++ 0x0D-structures_typedef/dev/4-new_dog.c
@@ -13,16 +13,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 
 	d = malloc(sizeof(struct dog));
-	if (!d)
-	{
-		free(d);
-		return (NULL);
-	}
-	else
+	if (d)
 	{
 		d->name = name;
-		d->age = age;
-		d->owner = owner;
-		return (d);
+		if (d->name)
+		{
+			d->age = age;
+			d->owner = owner;
+			if (d->owner)
+				return (d);
+			else
+			{
+				free(d->owner);
+				free(d->name);
+			}
+		}
+		else
+			free(d->name);
 	}
+	free(d);
+	return (NULL);
 }
