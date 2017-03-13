abb15962398e3da00f6ff890c2458fe2e50728d8
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-13 22:17:05 +0000

    updated to malloc for each element of struct

--- 0x0D-structures_typedef/dev/4-new_dog.c
+++ 0x0D-structures_typedef/dev/4-new_dog.c
@@ -1,6 +1,19 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * my_strlen - finds string length in order to malloc properly
+ * @str: input string
+ * Return: string length integer
+ */
+int my_strlen(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+		i++;
+	return (i);
+}
 /**
  * new_dog - creates new type of struct dog
  * @name: dog name
@@ -10,27 +23,32 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
+	dog_t *snoop_dogg;
+	int name_len = my_strlen(name), owner_len = my_strlen(owner);
 
-	d = malloc(sizeof(struct dog));
-	if (d)
+	snoop_dogg = malloc(sizeof(struct dog));
+	if (snoop_dogg)
 	{
-		d->name = name;
-		if (d->name)
+		snoop_dogg->name = malloc(sizeof(char) * name_len);
+		if (snoop_dogg->name)
 		{
-			d->age = age;
-			d->owner = owner;
-			if (d->owner)
-				return (d);
+			snoop_dogg->name = name;
+			snoop_dogg->age = age;
+			snoop_dogg->owner = malloc(sizeof(char) * owner_len);
+			if (snoop_dogg->owner)
+			{
+				snoop_dogg->owner = owner;
+				return (snoop_dogg);
+			}
 			else
 			{
-				free(d->owner);
-				free(d->name);
+				free(snoop_dogg->owner);
+				free(snoop_dogg->name);
 			}
 		}
 		else
-			free(d->name);
+			free(snoop_dogg->name);
 	}
-	free(d);
+	free(snoop_dogg);
 	return (NULL);
 }
