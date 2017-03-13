24df5c0133945047c193896331cab0cdef821a21
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-13 22:30:11 +0000

    updated to copy strings instead of copying pointers

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
  * my_strlen - finds string length in order to malloc properly
  * @str: input string
@@ -15,6 +14,24 @@ int my_strlen(char *str)
 	return (i);
 }
 /**
+ * my_strcpy - copies string
+ * @to: pointer to new string
+ * @from: pointer to string to copy
+ * Return: 1 success, 0 fail
+ */
+int my_strcpy(char *to, char *from)
+{
+	int i, len = my_strlen(from);
+	to = malloc(sizeof(char) * len);
+	if (to)
+	{
+		for (i = 0; i < len; i++)
+			to[i] = from[i];
+		return (1);
+	}
+	return (0);
+}
+/**
  * new_dog - creates new type of struct dog
  * @name: dog name
  * @age: dog age
@@ -24,18 +41,15 @@ int my_strlen(char *str)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *snoop_dogg;
-	int name_len = my_strlen(name), owner_len = my_strlen(owner);
 
 	snoop_dogg = malloc(sizeof(struct dog));
 	if (snoop_dogg)
 	{
-		snoop_dogg->name = malloc(sizeof(char) * name_len);
-		if (snoop_dogg->name)
+		if (my_strcpy(snoop_dogg->name, name))
 		{
 			snoop_dogg->name = name;
 			snoop_dogg->age = age;
-			snoop_dogg->owner = malloc(sizeof(char) * owner_len);
-			if (snoop_dogg->owner)
+			if (my_strcpy(snoop_dogg->owner, owner))
 			{
 				snoop_dogg->owner = owner;
 				return (snoop_dogg);
