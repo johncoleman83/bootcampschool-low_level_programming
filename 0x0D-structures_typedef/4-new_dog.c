5c14641f8413f13595069fdb9089920afcf4e904
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-13 21:41:09 +0000

    reordered if statement

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,13 +13,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 
 	d = malloc(sizeof(struct dog));
-	if (d)
+	if (!d)
+	{
+		free(d);
+		return (NULL);
+	}
+	else
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 		return (d);
 	}
-	free(d);
-	return (NULL);
 }
