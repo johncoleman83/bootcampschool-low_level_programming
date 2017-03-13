47a3c665979f26af0e6ab0e047813ceeee4bfda6
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-13 20:40:57 +0000

    added malloc

--- 0x0D-structures_typedef/dev/1-init_dog.c
+++ 0x0D-structures_typedef/dev/1-init_dog.c
@@ -9,7 +9,13 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	d = malloc(sizeof(sturct dog));
+	if (d == NULL)
+		;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
