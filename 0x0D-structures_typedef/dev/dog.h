648ce1e62088ac8046c6142914ce2fa73938046c
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-14 01:45:21 +0000

    updated typede style

--- 0x0D-structures_typedef/dev/dog.h
+++ 0x0D-structures_typedef/dev/dog.h
@@ -7,13 +7,12 @@
  * @owner: dog owner
  * Description: defines dog types
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-typedef struct dog dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
