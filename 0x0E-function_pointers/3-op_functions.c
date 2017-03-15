78f4846955c92c6d326a2f8a69a6db3e8d56e704
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-15 23:20:18 +0000

    changed order of conditional and return statement

--- 0x0E-function_pointers/3-op_functions.c
+++ 0x0E-function_pointers/3-op_functions.c
@@ -37,12 +37,10 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a / b);
+	if (b != 0)
+		return (a / b);
+	printf("Error\n");
+	exit(100);
 }
 /**
  * op_mod - modulo of a / b
@@ -52,10 +50,8 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+	if (b != 0)
+		return (a % b);
+	printf("Error\n");
+	exit(100);;
 }
