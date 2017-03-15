7deac75a977322a91552b152d1d21d3174414169
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-15 22:47:00 +0000

    changed around the return on invalid operator

--- 0x0E-function_pointers/dev/3-main.c
+++ 0x0E-function_pointers/dev/3-main.c
@@ -9,8 +9,16 @@ int main(int argc, char **argv)
 {
 	if (argc == 4)
 	{
-		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-		return (0);
+		if (get_op_func(argv[2]))
+		{
+			printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+			return (0);
+		}
+		else
+		{
+			printf("Error\n");
+			return (99);
+		}
 	}
 	printf("Error\n");
 	return (98);
