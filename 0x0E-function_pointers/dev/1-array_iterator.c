5ac204cc7d15a63ad5d8400205b8a6b292b451dd
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-15 20:20:30 +0000

    accounted for NULL inputs

--- 0x0E-function_pointers/dev/1-array_iterator.c
+++ 0x0E-function_pointers/dev/1-array_iterator.c
@@ -10,9 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	while (i < size)
-	{
-		action(array[i]);
-		i++;
-	}
+	if (array && size && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
