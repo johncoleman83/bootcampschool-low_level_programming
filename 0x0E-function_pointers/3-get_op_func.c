965f0114f8b18a0b680b82435b9da31579b60dbf
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-15 22:35:43 +0000

    proper syntax

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -6,15 +6,15 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
 	while (ops[i].op)
 	{
