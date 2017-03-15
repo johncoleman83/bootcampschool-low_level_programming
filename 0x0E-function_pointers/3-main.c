cc683d64f5f86274d6a83f6686b276675e1ceb5d
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-15 23:41:17 +0000

    changed to check for NULL return because it was uneccessary code

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -9,16 +9,8 @@ int main(int argc, char **argv)
 {
 	if (argc == 4)
 	{
-		if (get_op_func(argv[2]))
-		{
-			printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-			return (0);
-		}
-		else
-		{
-			printf("Error\n");
-			return (99);
-		}
+		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+		return (0);
 	}
 	printf("Error\n");
 	return (98);
