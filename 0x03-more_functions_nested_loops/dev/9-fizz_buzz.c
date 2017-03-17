c941d5a7936c7ce926ad029a34b254b92038a7e0
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-17 02:38:33 +0000

    optimized Fizz buzz

--- 0x03-more_functions_nested_loops/dev/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/dev/9-fizz_buzz.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
+ * Description: Classic FizzBuzz problem
  * Return: Always 0, Success!
  */
 int main(void)
 {
-	int i = 1;
+    int i;
+    char result[4][9] = {"%d", "Fizz", "Buzz", "FizzBuzz"};
 
-	while (i <= 100)
-	{
-		if (i % 3 == 0)
-			printf("Fizz");
-		if (i % 5 == 0)
-			printf("Buzz");
-		if (i % 3 != 0 && i % 5 != 0)
-			printf("%d", i);
-		if (i < 100)
-			printf(" ");
-		i++;
-	}
-	printf("\n");
+    for (i = 1; i < 101; i++)
+    {
+        printf(result[(i % 3 == 0) + (2 * (i % 5 == 0))], i);
+        printf(i < 100 ? " " : "\n");
+    }
 	return (0);
 }
