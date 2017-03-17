6a714254a357268bb7bf5d607b2730df4d3779bb
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-17 22:55:23 +0000

    changed orders boolean

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -14,36 +14,32 @@ void print_all(const char * const format, ...)
 	va_start(valist, format);
 	while (format[i])
 	{
-		orders = 0;
+		orders = 1;
 		switch (format[i])
 		{
 		case 'c' :
 			temp_c = va_arg(valist, int);
 			printf("%c", temp_c);
-			orders = 1;
 			break;
 		case 'i' :
 			temp_d = va_arg(valist, int);
 			printf("%d", temp_d);
-			orders = 1;
 			break;
 		case 'f' :
 			temp_f = va_arg(valist, double);
 			printf("%lf", temp_f);
-			orders = 1;
 			break;
 		case 's' :
 			temp_s = va_arg(valist, char*);
 			if (temp_s)
 			{
 				printf("%s", temp_s);
-				orders = 1;
 				break;
 			}
 			printf("(nil)");
-			orders = 1;
 			break;
 		default :
+			orders = 0;
 			break;
 		}
 		if (format[i + 1] && orders)
