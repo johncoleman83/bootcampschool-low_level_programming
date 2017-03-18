7e604572ce55a6eb09cde12c8e728ac11a85da5a
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-18 01:10:45 +0000

    updated to check for input of NULL for format and removed unnecessary variables

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -7,49 +7,41 @@
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	char *temp_s, temp_c;
-	float temp_f;
-	int temp_d, i = 0, orders;
+	char *temp_s;
+	int i = 0, orders;
 
-	while (format)
-	{
 	va_start(valist, format);
-		while (format[i])
+	while (format && format[i])
+	{
+		orders = 1;
+		switch (format[i])
 		{
-			orders = 1;
-			switch (format[i])
+		case 'c':
+			printf("%c", va_arg(valist, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int));
+			break;
+		case 'f':
+			printf("%g", va_arg(valist, double));
+			break;
+		case 's':
+			temp_s = va_arg(valist, char*);
+			if (temp_s)
 			{
-			case 'c' :
-				temp_c = va_arg(valist, int);
-				printf("%c", temp_c);
-				break;
-			case 'i' :
-				temp_d = va_arg(valist, int);
-				printf("%d", temp_d);
-				break;
-			case 'f' :
-				temp_f = va_arg(valist, double);
-				printf("%g", temp_f);
-				break;
-			case 's' :
-				temp_s = va_arg(valist, char*);
-				if (temp_s)
-				{
-					printf("%s", temp_s);
-					break;
-				}
-				printf("(nil)");
-				break;
-			default :
-				orders = 0;
+				printf("%s", temp_s);
 				break;
 			}
-			if (format[i + 1] && orders)
-				printf(", ");
-			i++;
+			printf("(nil)");
+			break;
+		default:
+			orders = 0;
+			break;
 		}
-		va_end(valist);
-		break;
+		if (format[i + 1] && orders)
+			printf(", ");
+		i++;
 	}
+	va_end(valist);
 	printf("\n");
 }
