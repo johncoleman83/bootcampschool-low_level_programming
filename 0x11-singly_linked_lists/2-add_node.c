d45f5e52aaa079b830561f9756037e695cdeefde
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-23 21:37:00 +0000

    removed leading spaces

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -24,9 +24,9 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
-    {
+	{
 		return (0);
-    }
+	}
 	new_node->str = strdup(str);
 	new_node->len = length;
 	new_node->next = *head;
