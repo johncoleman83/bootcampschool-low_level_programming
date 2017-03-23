60ba5d8c42bcd881de0a9665ae2bcc24af71b85e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-23 22:35:16 +0000

    updated 2 to change order of pointers

--- 0x11-singly_linked_lists/dev/2-add_node.c
+++ 0x11-singly_linked_lists/dev/2-add_node.c
@@ -20,15 +20,14 @@ int _strlen(const char *s)
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	unsigned int length = _strlen(str);
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
 
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 	new_node->str = strdup(str);
-	new_node->len = length;
+	new_node->len = _strlen(str);
 	new_node->next = *head;
+	*head = new_node;
 	return (new_node);
 }
