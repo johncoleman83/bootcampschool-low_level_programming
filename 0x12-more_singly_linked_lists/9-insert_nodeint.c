bd5164ff1d9095fcf23c25d3b82e578bc368c163
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-28 18:01:14 +0000

    updated to account for index of 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = NULL, *temp_node = *head;
+	listint_t *new_node, *temp_node = *head;
 	unsigned int i = 0;
 
 	while (temp_node && i < idx - 1)
@@ -16,20 +16,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp_node = temp_node->next;
 		i++;
 	}
-	if (temp_node || i >= idx - 1)
+	if (temp_node || (i == idx - 1 || idx == 0))
 	{
 		new_node = malloc(sizeof(listint_t));
-		new_node->n = n;
-		if (idx == 0)
+		if (new_node)
 		{
-			new_node->next = NULL;
-			temp_node = new_node;
-		}
-		else
-		{
-			new_node->next = temp_node->next;
-			temp_node->next = new_node;
+			new_node->n = n;
+			if (idx == 0)
+			{
+				new_node->next = *head;
+				*head = new_node;
+			}
+			else
+			{
+				new_node->next = temp_node->next;
+				temp_node->next = new_node;
+			}
+			return (new_node);
 		}
 	}
-	return (new_node);
+	return (NULL);
 }
--- 0x12-more_singly_linked_lists/dev/9-main.c
+++ 0x12-more_singly_linked_lists/dev/9-main.c
@@ -23,7 +23,8 @@ int main(void)
     add_nodeint_end(&head, 1024);
     print_listint(head);
     printf("-----------------\n");
-    insert_nodeint_at_index(&head, 5, 4096);
+    insert_nodeint_at_index(&head, 5, 40965);
+	insert_nodeint_at_index(&head, 0, 40960);
     print_listint(head);
     free_listint2(&head);
     return (0);
