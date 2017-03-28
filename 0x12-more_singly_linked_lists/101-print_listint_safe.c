8900af84dccabee75c0782b990c9484697740ef5
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-28 21:44:18 +0000

    added check for end next pointer that points to middle of list

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints linked list
+ * @head: beginning of linked list
+ * Return: number of elements in linked list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i = 0, j;
+	const listint_t *temp_h = head, *check_next;
+
+	while (temp_h)
+	{
+		printf("%d\n", temp_h->n);
+		i++;
+		temp_h = temp_h->next;
+		check_next = head;
+		j = 0;
+		while (j < i)
+		{
+			if (temp_h == check_next)
+				return (i);
+			check_next = check_next->next;
+			j++;
+		}
+	}
+	return (i);
+}
