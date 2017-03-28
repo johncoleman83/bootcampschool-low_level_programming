29209f4bab7de84706bc53a075e857491691fb13
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-28 20:04:04 +0000

    delete node at input index

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,5 +7,31 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	return ();
+	listint_t *index_node, *temp_node = *head;
+	unsigned int i = 0;
+
+	while (temp_node && i < index)
+	{
+		temp_node = temp_node->next;
+		i++;
+	}
+	if (temp_node && i == index)
+	{
+		if (index != 0)
+		{
+			index_node = temp_node->next;
+			if (index_node)
+			{
+				temp_node->next = index_node->next;
+				free(index_node);
+			}
+		}
+		else
+		{
+			*head = temp_node->next;
+			free (temp_node);
+		}
+		return (1);
+	}
+	return (-1);
 }
