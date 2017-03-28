a8fe4230fb786e51790f6618cd892f5177f8c02e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-28 21:05:09 +0000

    reordered while loop

--- 0x12-more_singly_linked_lists/dev/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/dev/10-delete_nodeint.c
@@ -10,28 +10,26 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *index_node, *temp_node = *head;
 	unsigned int i = 0;
 
-	while (temp_node && i < index)
+	if (temp_node && (index == 0))
 	{
-		temp_node = temp_node->next;
-		i++;
+		*head = temp_node->next;
+		free(temp_node);
+		return (1);
 	}
-	if (temp_node && i == index)
+	while (temp_node)
 	{
-		if (index != 0)
+		if (i + 1 == index)
 		{
 			index_node = temp_node->next;
 			if (index_node)
 			{
 				temp_node->next = index_node->next;
 				free(index_node);
+				return (1);
 			}
 		}
-		else
-		{
-			*head = temp_node->next;
-			free (temp_node);
-		}
-		return (1);
+		temp_node = temp_node->next;
+		i++;
 	}
 	return (-1);
 }
