878c935f34947767f2db582389bd70668cc702ff
johncoleman83 <lcsw@davidjohncoleman.com>
2017-03-28 22:13:28 +0000

    changed if check statement

--- 0x12-more_singly_linked_lists/dev/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/dev/100-reverse_listint.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 /**
- *  - 
- * @: 
- * @: 
- * Return: 
+ * reverse_listint - reverses linked list
+ * @head: beginning of linked list
+ * Return: pointer to first node of reversed list
  */
-
+listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *find_next, *temp_h = *head;
+
+	
 	return ();
 }
