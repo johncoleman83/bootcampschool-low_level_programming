bd22296b245bf65bb80500d8c46de6c73287f384
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-04 16:56:37 +0000

    accounted for input of NULL string for file contents

--- 0x14-file_io/dev/1-create_file.c
+++ 0x14-file_io/dev/1-create_file.c
@@ -26,6 +26,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 	len = _strlen(text_content);
 	if (write(fd, text_content, len) == -1)
 		return (-1);
