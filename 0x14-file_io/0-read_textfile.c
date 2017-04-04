377fbcd01e9dd97f5c6a8e8ae11fae2e7cff6c62
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-04 20:47:36 +0000

    added if conditional to check for NULL input of filename

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -24,6 +24,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t letters2;
 	char *buffer;
 
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
