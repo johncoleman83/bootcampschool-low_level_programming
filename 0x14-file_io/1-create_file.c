f1636c3f86335d7f6a81f73ff54b446d3c8090f0
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-04 17:32:55 +0000

    added close to all return cases

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -27,9 +27,16 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
+	}
 	len = _strlen(text_content);
 	if (write(fd, text_content, len) == -1)
+	{
+		close(fd);
 		return (-1);
+	}
+	close(fd);
 	return (1);
 }
