5dda1461ef6449ee21b8393dbf33cc3b3517d34e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-04 17:41:19 +0000

    appends to a file

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -1,5 +1,18 @@
 #include "holberton.h"
 /**
+ * _strlen - returns string length of input string
+ * @s: string to check length of
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
  * append_text_to_file - appends text to the end of a file
  * @filename: name of file to have text appended to
  * @text_content: text to be appended to the file
@@ -7,5 +20,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	return ();
+	int fd;
+	size_t len;
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	len = _strlen(text_content);
+	if (write(fd, text_content, len) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
--- 0x14-file_io/dev/2-append_text_to_file.c
+++ 0x14-file_io/dev/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	size_t len;
 
-	fd = open(filename, O_TRUNC);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
