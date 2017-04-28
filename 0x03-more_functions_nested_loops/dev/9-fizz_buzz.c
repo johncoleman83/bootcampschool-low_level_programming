2cdb6e7698ba4aefd248714ac19135d2fd8c0fd0
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-28 09:04:55 -0700

    updated tabs

--- 0x03-more_functions_nested_loops/dev/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/dev/9-fizz_buzz.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-    int i;
-    char result[4][9] = {"%d", "Fizz", "Buzz", "FizzBuzz"};
+	int i;
+	char result[4][9] = {"%d", "Fizz", "Buzz", "FizzBuzz"};
 
-    for (i = 1; i < 101; i++)
-    {
-        printf(result[(i % 3 == 0) + (2 * (i % 5 == 0))], i);
-        printf(i < 100 ? " " : "\n");
-    }
+	for (i = 1; i < 101; i++)
+	{
+		printf(result[(i % 3 == 0) + (2 * (i % 5 == 0))], i);
+		printf(i < 100 ? " " : "\n");
+	}
 	return (0);
 }
