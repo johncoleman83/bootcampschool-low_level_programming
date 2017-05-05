006c154e3d438b0f253ecf6ca16837ef96b0c5cf
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-05 23:56:38 +0000

    initiailzing directory

---
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,7 @@
+#include "lists.h"
+/**
+ *  -
+ * h: head of linked list
+ *
+ * Return: 
+ */
--- 0x16-doubly_linked_lists/dev/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/dev/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * print_dlist - prints all the elements of a dlistint_t list
+ * h: head of linked list
+ *
+ * Return: number of elements in linked list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *copy = h;
