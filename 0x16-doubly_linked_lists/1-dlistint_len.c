7d89099b03273161fc43f96b33665ba6fb12b82a
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-06 00:10:10 +0000

    print list elements and return number of elements

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,20 @@
 #include "lists.h"
 /**
- *  -
+ * dlistint_len - returns the number of elements in a linked list
  * h: head of linked list
  *
- * Return: 
+ * Return: number of elements in linked list
  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *copy = h;
+	size_t i = 0;
+
+	while (copy)
+	{
+		copy = copy->next;
+		i++;
+	}
+
+	return (i);
+}
