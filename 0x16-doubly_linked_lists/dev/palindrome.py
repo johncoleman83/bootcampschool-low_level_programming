78a143fe1988211d190ccb8853fa44eb80ae8273
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-08 05:28:27 +0000

    updated algorithm to find sum

--- 0x16-doubly_linked_lists/dev/palindrome.py
+++ 0x16-doubly_linked_lists/dev/palindrome.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 
 
+def sumofthreedignums(thesum, themin):
+    a = 999
+    while a > 99:
+        b = 999
+        while a * b > themin:
+            b -= 1
+            if a * b == thesum:
+                return True
+        a -= 1
+    return False
+
+
 def main():
-    maxsum = 999 * 999
-    while str(maxsum) != str(maxsum)[-1::-1]:
-        maxsum -= 1
-    print(maxsum)
+    thesum = 999 * 999
+    themin = 999 * 100 - 1
+    while thesum > themin:
+        if str(thesum) == str(thesum)[-1::-1]:
+            if sumofthreedignums(thesum, themin):
+                break
+        thesum -= 1
+    print(thesum)
 
 
 if __name__ == "__main__":
