87ee677f42afc442688f9de4f9baf5ecd47e91cf
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-08 16:52:46 +0000

    updated for accounting for previous node

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
