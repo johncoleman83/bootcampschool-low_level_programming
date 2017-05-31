5408a8e159f4b0ab688c52aa28d709b9ebe404ae
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-31 19:34:20 +0000

    updated dev directory and completed hash index function

---
+++ 0x19-hash_tables/dev/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates hash table
+ * @size: size of hash table
+ *
+ * Return: pointer to hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	hash_node_t *s;
+
+	if (!size)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t) * 1);
+	if (!ht)
+		return (NULL);
+
+	s = calloc(size, sizeof(hash_node_t *));
+	if (!s)
+	{
+		free(ht);
+		return (NULL);
+	}
+	ht->array = &s;
+	ht->size = size;
+
+	return (ht);
+}
