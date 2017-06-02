9f695e656e9684ae607f3cd76212043dbedb8667
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-02 05:55:52 +0000

    assigned next variable a value

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,36 +13,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int hash, index, i, size;
 	hash_node_t *node, *new_node;
 
-	if (!ht || !key || strlen(key) == 0)
+	if (!ht || !key || !key || strlen(key) == 0)
 		return (EXIT_FAILURE);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
-	if (!node)
-	{
-		new_node = malloc(sizeof(hash_node_t *));
-		if (!new_node)
-			return (EXIT_FAILURE);
-		new_node->key = strdup(key);
-		new_node->value = strdup(value);
-		ht->array[index] = new_node;
-	}
-	else
+	if (node)
 	{
 		if (strncmp(node->key, key, strlen(key) == 0))
 		{
 			free(node->key);
 			node->value = strdup(value);
 		}
-		else
-		{
-			new_node = malloc(sizeof(hash_node_t *));
-			if (!new_node)
-				return (EXIT_FAILURE);
-			new_node->key = strdup(key);
-			new_node->value = strdup(value);
-			new_node->next = node;
-		}
+	}
+	else
+	{
+		new_node = malloc(sizeof(hash_node_t *));
+		if (!new_node)
+			return (EXIT_FAILURE);
+		new_node->key = strdup(key);
+		new_node->value = strdup(value);
+		new_node->next = node;
+		ht->array[index] = new_node;
 	}
 	return (EXIT_SUCCESS);
 }
