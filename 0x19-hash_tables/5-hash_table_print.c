ffafa702c9031e6acd2041732321d97b225dc5cc
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-02 22:22:06 +0000

    created print function to print hash table key value pairs

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -9,4 +9,39 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *node;
+	unsigned long int i, size;
+	int comma = 0, start = 1;
+
+	if (ht && ht->array)
+	{
+		size = ht->size;
+		i = 0;
+		while (i < size)
+		{
+			node = (ht->array)[i];
+			if (node)
+			{
+				if (comma)
+					printf(", ");
+				if (start)
+				{
+					putchar('{');
+					start = 0, comma = 1;
+				}
+				if (node->next)
+				{
+					while (node->next)
+					{
+						printf("'%s': '%s', ", node->key, node->value);
+						node = node->next;
+					}
+				}
+				printf("'%s': '%s'", node->key, node->value);
+			}
+			i++;
+		}
+		if (!start)
+			printf("}\n");
+	}
 }
