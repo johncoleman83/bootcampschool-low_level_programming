356726a4332312b22fd74e86ccf7aed413c4d25d
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-15 19:18:20 +0000

    updated for style and to check for NULL input

--- 0x1A-sorting_algorithms/100-shell_sort.c
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -2,48 +2,43 @@
 
 /**
  * shell_sort - sorts list with shell sort method
- * @list: input list to sort
+ * @array: input list to sort
  * @size: lenth/ size of input array
  */
 void shell_sort(int *array, size_t size)
 {
 	int *gaparr, gaps, gap, g;
-	size_t check, prev;
-	size_t sum;
-
-	gaps = 0;
-	for (sum = 1; sum <= size / 2; sum = sum * 3 + 1)
-		gaps++;
+	size_t check, prev, sum;
 
 	if (array && size > 1)
 	{
-		gaparr = malloc(sizeof(int) * gaps);
-
-		for (sum = 1, g = 0; g < gaps; sum = sum * 3 + 1, g++)
-			gaparr[g] = sum;
-
-		prev = 0;
-		gap = gaparr[--g];
-		check = prev + gap;
-
-		while (check < size)
+		gaps = 0;
+		for (sum = 1; sum <= size / 2; sum = sum * 3 + 1)
+			gaps++;
+		if (array && size > 1)
 		{
+			gaparr = malloc(sizeof(int) * gaps);
 
-			if (array[check] < array[prev])
+			for (sum = 1, g = 0; g < gaps; sum = sum * 3 + 1, g++)
+				gaparr[g] = sum;
+			prev = 0;
+			gap = gaparr[--g];
+			check = prev + gap;
+			while (check < size)
 			{
-				swap_shell(check, prev, gap, array);
-			}
-			prev++;
-			check++;
-
-			if (check >= size)
-			{
-				print_array(array, size);
-				if (g != 0)
+				if (array[check] < array[prev])
+					swap_shell(check, prev, gap, array);
+				prev++;
+				check++;
+				if (check >= size)
 				{
-					gap = gaparr[--g];
-					prev = 0;
-					check = prev + gap;
+					print_array(array, size);
+					if (g != 0)
+					{
+						gap = gaparr[--g];
+						prev = 0;
+						check = prev + gap;
+					}
 				}
 			}
 		}
@@ -56,7 +51,7 @@ void shell_sort(int *array, size_t size)
  * @gap: gap to rotate check and prev
  * @a: the array for referencing indexes
  */
-void swap_shell(int check, int prev, int gap, int*a)
+void swap_shell(int check, int prev, int gap, int *a)
 {
 	int temp;
 
