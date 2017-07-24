a4c0f125d9e7944e508cda023384bb1b73c51c1e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-24 15:27:20 -0700

    made all lines less than 80 chars

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - inserts new leaf node in binary tree based on given parent
- * left and right nodes of new leaf node having NULL value
+ * binary_tree_node - inserts new leaf node in binary tree based on given
+ * parent, with left and right nodes of new leaf node having NULL value
  * @parent: parent node of new leaf in binary tree
  * @value: the value of the integer in the new leaf
  *
--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_leaf - checks if input node is a leaf node
+ * @node: node to check if it is a leaf
  *
+ * Return: 1 if leaf, 0 if not or upon failure
  */
-int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	return (node && (!node->left && !node->right));
+}
