79be1355f4fee068d73ad897ec3ca5807be95e55
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-24 15:15:35 -0700

    refactored code to remove unneccesary variables

--- 0x1C-binary_trees/3-binary_tree_delete.c
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -6,14 +6,10 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *left, *right;
-
 	if (tree)
 	{
-		left = tree->left;
-		right = tree->right;
-		binary_tree_delete(right);
-		binary_tree_delete(left);
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
 		free(tree);
 	}
 }
