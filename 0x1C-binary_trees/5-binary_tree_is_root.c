aa5804e4194f69d2d7038435136dd954d688d234
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-24 15:35:14 -0700

    checks if input node is root or not

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_root - checks if input node is the root of tree
+ * @node: input node to check if it is root of a tree
  *
+ * Return: 1 if node is root, 0 if not or upon failure
  */
-int binary_tree_is_root(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	return (node && !node->parent);
+}
