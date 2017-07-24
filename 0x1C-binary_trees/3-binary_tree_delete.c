b4da89f9f52db2e8bfa3d27f1d399de313c31b67
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-24 15:06:58 -0700

    frees entire binary tree

--- 0x1C-binary_trees/3-binary_tree_delete.c
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -1,5 +1,19 @@
 #include "binary_trees.h"
+
 /**
- *
+ * binary_tree_delete - deletes entire binary tree, use with caution!
+ * @tree: the binary tree to delete
  */
-void binary_tree_delete(binary_tree_t *tree);
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *left, *right;
+
+	if (tree)
+	{
+		left = tree->left;
+		right = tree->right;
+		binary_tree_delete(right);
+		binary_tree_delete(left);
+		free(tree);
+	}
+}
