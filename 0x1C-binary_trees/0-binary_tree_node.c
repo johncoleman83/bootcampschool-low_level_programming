063411dbdad1d02eaf32c821d3aa1aa05f9acb52
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-24 14:43:09 -0700

    added newlines for more clear style

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,9 +1,11 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_node - inserts new leaf node in binary tree based on given parent
  * left and right nodes of new leaf node having NULL value
  * @parent: parent node of new leaf in binary tree
  * @value: the value of the integer in the new leaf
+ *
  * Return: pointer to new leaf or NULL
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
