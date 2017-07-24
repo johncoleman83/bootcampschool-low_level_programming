7aa96c0e50c87933cdda227361e0f2a90d76fc95
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-24 16:09:26 -0700

    traverse through binary tree with inorder taverse algorithm

--- 0x1C-binary_trees/7-binary_tree_inorder.c
+++ 0x1C-binary_trees/7-binary_tree_inorder.c
@@ -1,5 +1,15 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_inorder - traverses binary tree with inorder functionality
+ * @tree: root of binary tree to traverse
+ * @func: pointer to function to use on each integer
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
