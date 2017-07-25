778f2e53f667a7deec2cd11066a58bb5d7079386
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-25 15:25:37 -0700

    updated Return statement in comments

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -3,7 +3,7 @@
  * binary_tree_sibling - finds the sibling of input node
  * @n: node to check for a sibling
  *
- * Returns: pointer to sibling or NULL if none or failure
+ * Return: pointer to sibling or NULL if none or failure
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *n)
 {
--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 /**
- * binary_tree_sibling - finds the sibling of input node
+ * bt_sibling - finds the sibling of input node
  * @n: node to check for a sibling
  *
- * Returns: pointer to sibling or NULL if none or failure
+ * Return: pointer to sibling or NULL if none or failure
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *n)
+binary_tree_t *bt_sibling(binary_tree_t *n)
 {
 	if (n && n->parent)
 		return (n->parent->right == n ? n->parent->left : n->parent->right);
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *n)
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node && node->parent)
-		return (binary_tree_sibling(node->parent));
+		return (bt_sibling(node->parent));
 	else
 		return (NULL);
 }
