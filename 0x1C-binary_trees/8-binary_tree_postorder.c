3336c9ed563d581788298274c1e53e947a0b2781
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-24 20:26:24 -0700

    calculates height of a binary tree

--- 0x1C-binary_trees/8-binary_tree_postorder.c
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -1,5 +1,15 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_postorder - traverses binary tree with postorder algorithm
+ * @tree: root of binary tree to traverse
+ * @func: pointer to function to use on each integer
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -1,5 +1,20 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - returns height of a binary tree
+ * @tree: root of binary tree to find the height of
  *
+ * Return: height of binary tree in size_t type
  */
-size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t r, l;
+
+	if (tree)
+	{
+		r = binary_tree_height(tree->right) + 1;
+		l = binary_tree_height(tree->left) + 1;
+		return (r > l ? r : l);
+	}
+	else
+		return (-1);
+}
