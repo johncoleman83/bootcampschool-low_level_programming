e24993483aac28f5c24f7972491759be5ceae1c0
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-25 14:49:21 -0700

    function returns the sibling node

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -1,5 +1,15 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_sibling - finds the sibling of input node
+ * @node: node to check for a sibling
  *
+ * Returns: pointer to sibling or NULL if none or failure
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node && node->parent)
+		return (node->parent->right == node ? node->parent->left :
+				node->parent->right);
+	else
+		return (NULL);
+}
