b1f2c1213cbfc0e7984b74a25426805332fb1d38
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-25 07:58:56 -0700

    calculates # of leaves in a binary tree

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -1,5 +1,16 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_leaves - calculates the number of leaves in a binary tree
+ * @tree: the binary tree to check if there are leaves in it
  */
-size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree)
+	{
+		leaves += binary_tree_leaves(tree->right);
+		leaves += binary_tree_leaves(tree->left);
+	}
+	return (leaves + (tree && (!tree->left && !tree->right)));
+}
