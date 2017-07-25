92534ef41941473c5550e0d8915f14cd76ab1c60
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-25 07:47:13 -0700

    find size of binary tree

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdio.h>
 /**
  * binary_tree_size - calculates the size of a binary tree
  * @tree: the root of tree to measure the size of
@@ -8,15 +7,14 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t r, l, sum;
+	size_t r, l;
 
 	if (tree)
 	{
-		r = binary_tree_size(tree->right) + 1;
-		l = binary_tree_size(tree->left) + 1;
-		sum = r + l - (tree->parent ? 0 : 1);
-		return (sum ? sum : 1);
+		r = binary_tree_size(tree->right);
+		l = binary_tree_size(tree->left);
+		return (r + l + 1);
 	}
 	else
-		return (-1);
+		return (0);
 }
