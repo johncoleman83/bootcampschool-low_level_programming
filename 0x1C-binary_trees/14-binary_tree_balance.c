680b7f8d4649e3850e3c2082cbf3b7cdc8102e38
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-25 11:01:39 -0700

    added NULL check to binary height calculation

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,5 +1,40 @@
 #include "binary_trees.h"
 /**
+ * bt_height - returns height of a binary tree
+ * @tree: root of binary tree to find the height of
  *
+ * Return: height of binary tree in size_t type
+ */
+size_t bt_height(const binary_tree_t *tree)
+{
+	size_t r, l;
+
+	if (tree)
+	{
+		r = bt_height(tree->right) + 1;
+		l = bt_height(tree->left) + 1;
+		return (r > l ? r : l);
+	}
+	else
+		return (-1);
+}
+/**
+ * binary_tree_balance - returns balance factor of binary tree
+ * that is, the height of tree->left - height of tree->right
+ * @tree: root of binary tree to find the balance factor
+ *
+ * Return: height of binary tree in size_t type
  */
 int binary_tree_balance(const binary_tree_t *tree);
+{
+	int balance = 0;
+	size_t r, l;
+
+	if (tree)
+	{
+		l = bt_height(tree->left);
+		r = bt_height(tree->right);
+		balance = l - r;
+	}
+	return (balance);
+}
--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -1,5 +1,24 @@
 #include "binary_trees.h"
 /**
+ * bt_height - returns height of a binary tree
+ * @tree: root of binary tree to find the height of
+ *
+ * Return: height of binary tree in size_t type
+ */
+size_t bt_height(const binary_tree_t *tree)
+{
+	size_t r, l;
+
+	if (tree)
+	{
+		r = bt_height(tree->right) + 1;
+		l = bt_height(tree->left) + 1;
+		return (r > l ? r : l);
+	}
+	else
+		return (-1);
+}
+/**
  * binary_tree_height - returns height of a binary tree
  * @tree: root of binary tree to find the height of
  *
@@ -7,14 +26,8 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t r, l;
-
 	if (tree)
-	{
-		r = binary_tree_height(tree->right) + 1;
-		l = binary_tree_height(tree->left) + 1;
-		return (r > l ? r : l);
-	}
+		return (bt_height(tree));
 	else
-		return (-1);
+		return (0);
 }
