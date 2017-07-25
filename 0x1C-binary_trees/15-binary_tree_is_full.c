b81742f661d1bb364fd9f806143a26c4c59d4815
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-25 12:33:36 -0700

    calculates if a binary tree is full, containing no nodes with only 1 child

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
 /**
+ * asym_node - calculates the number of asymetrical nodes in a binary tree
+ * @tree: the binary tree to check if there are nodes
+ *
+ * Return: the number of asymetrical nodes
+ */
+size_t asym_node(const binary_tree_t *tree)
+{
+	size_t nodes = 0, asym = 0;
+
+	if (tree)
+	{
+		nodes += asym_node(tree->right);
+		nodes += asym_node(tree->left);
+		asym += ((tree->left && !tree->right) || (tree->right && !tree->left));
+	}
+	return (nodes + (tree && asym));
+}
+/**
  * binary_tree_is_full - checks if a binary tree is full
  * @tree: the input binary tree to check if it's full
  *
@@ -7,5 +25,5 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	
+	return (asym_node(tree) == 0);
 }
