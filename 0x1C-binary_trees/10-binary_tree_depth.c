5ddb32fd185f5092094b9f30ffee9693e8532afe
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-24 20:48:56 -0700

    calculates the depth of a binary tree from the input node

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -1,5 +1,16 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_depth - returns depth of input node
+ * @node: input node to check the depth of
  *
+ * Return: depth of the input node
  */
-size_t binary_tree_depth(const binary_tree_t *node);
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	size_t depth;
+
+	for (depth = 0; node; depth++)
+		node = node->parent;
+
+	return (depth - 1);
+}
