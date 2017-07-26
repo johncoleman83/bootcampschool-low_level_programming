c44a29bf10183e8c94178f22c0bc26a2f4c53268
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-25 21:48:10 -0700

    updated level order traversal to properly handle the root input

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 /**
- * binary_tree_levelorder - parses binary tree in level-order traversal
+ * btl - traverses binary tree recursively, in level-order skipping root
+ * @tree: the binary tree to parse
+ * @func: the function to implement on the n int of each node
+ */
+void btl(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		if (tree->left)
+			func(tree->left->n);
+		if (tree->right)
+			func(tree->right->n);
+		btl(tree->left, func);
+		btl(tree->right, func);
+	}
+}
+/**
+ * binary_tree_levelorder - prints root and begins traversing
  * @tree: the binary tree to parse
  * @func: the function to implement on the n int of each node
  */
@@ -8,13 +25,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree)
 	{
-		if (!tree->parent)
-			func(tree->n);
-		if (tree->left)
-			func(tree->left->n);
-		if (tree->right)
-			func(tree->right->n);
-		binary_tree_levelorder(tree->left, func);
-		binary_tree_levelorder(tree->right, func);
+		func(tree->n);
+		btl(tree, func);
 	}
 }
--- 0x1C-binary_trees/102-binary_tree_is_complete.c
+++ 0x1C-binary_trees/102-binary_tree_is_complete.c
@@ -1,7 +1,42 @@
 #include "binary_trees.h"
-
-
 /**
- *
+ * binary_tree_levelorder - parses binary tree in level-order traversal
+ * @tree: the binary tree to parse
+ * @func: the function to implement on the n int of each node
  */
-int binary_tree_is_complete(const binary_tree_t *tree);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		if (!tree->parent)
+			func(tree->n);
+		if (tree->left)
+			func(tree->left->n);
+		if (tree->right)
+			func(tree->right->n);
+		binary_tree_levelorder(tree->left, func);
+		binary_tree_levelorder(tree->right, func);
+	}
+}
+int left_leaf_count(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree)
+	{
+		leaves += binary_tree_leaves(tree->right);
+		leaves += binary_tree_leaves(tree->left);
+	}
+	return (leaves + (tree && (!tree->left && !tree->right)));
+}
+/**
+ * binary_tree_is_complete - checks if binary tree is complete
+ * @tree: the binary tree to check
+ *
+ * Return: 1 if complete, 0 if not or upon error
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	check balance factor of every node, if all are 0 or +1, gtg.
+	return ();
+}
