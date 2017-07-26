a7bcf726bc099f37c9bfa1b385e490822de949e2
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-25 19:41:13 -0700

    operates a function on each node in binary tree using level-order traversal

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -1,5 +1,20 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_levelorder - parses binary tree in level-order traversal
+ * @tree: the binary tree to parse
+ * @func: the function to implement on the n int of each node
  */
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		if (!tree->parent)
+			func(tree->n);
+		if (tree->left)
+			func(tree->left->n);
+		if (tree->right)
+			func(tree->right->n);
+		binary_tree_levelorder(tree->left, func);
+		binary_tree_levelorder(tree->right, func);
+	}
+}
