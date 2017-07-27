6ebde9d84f82924a493634d46a933b0f0eefa4e5
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-26 22:07:21 -0700

    worst abuse of ternaries, just for fun

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -7,9 +7,8 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree || (tree->left && !tree->right) || (tree->right && !tree->left))
-		return (0);
-	if (!tree->left && !tree->right)
-		return (1);
-	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return ((!tree || (tree->left && !tree->right) ||
+			 (tree->right && !tree->left)) == 1 ? 0 :
+			(!tree->left && !tree->right ? 1 : binary_tree_is_full(tree->left)
+			 && binary_tree_is_full(tree->right)));
 }
