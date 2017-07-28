5cc2a8aa609487f59d51b2271e4eb7930cde47a5
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-27 22:09:13 -0700

    function checks if tree is AVL tree, meaning it is a bst and all nodes are balanced

--- 0x1C-binary_trees/120-binary_tree_is_avl.c
+++ 0x1C-binary_trees/120-binary_tree_is_avl.c
@@ -1,92 +1,89 @@
 #include "binary_trees.h"
 /**
- * bst_pop_queue - pops the head of the queue, in FIFO method
- * @queue: the queue to have an item pop'd
+ * bt_height - returns height of a binary tree + 1, so leaves have height 1, &
+ * NULL value is distinguishable from root leaf node by having height 0
+ * @tree: root of binary tree to find the height of
  *
- * Return: 1 if pop'd, 0 if nothing
+ * Return: height of binary tree in size_t type + 1
  */
-int bst_pop_queue(bt_queue_t **queue)
+size_t bt_height(const binary_tree_t *tree)
 {
-	bt_queue_t *temp;
-
-	if (queue && *queue)
-	{
-		temp = *queue;
-		*queue = temp->next;
-		free(temp);
-		return (1);
-	}
-	return (0);
-}
-/**
- * bst_push_queue - adds new binary tree node to end of queue FIFO method
- * @queue: the queue to add to
- * @bt_node: the binary tree node to add to the end of the queue
- *
- * Return: returns 0 on success, 1 on failure
- */
-int bst_push_queue(bt_queue_t **queue, const binary_tree_t *bt_node)
-{
-	bt_queue_t *new_node;
-
-	if (bt_node)
-	{
-		new_node = malloc(sizeof(bt_queue_t) * 1);
-		if (new_node)
-		{
-			new_node->node = bt_node;
-			new_node->next = NULL;
-			if (!*queue)
-				*queue = new_node;
-			else
-			{
-				if ((*queue)->node->n >= bt_node->n)
-				{
-					free(new_node);
-					bst_pop_queue(queue);
-					return (1);
-				}
-				(*queue)->next = new_node;
-				bst_pop_queue(queue);
-			}
-			return (0);
-		}
-	}
-	return (1);
-}
-/**
- * bst - traverses binary tree with inorder functionality, checking prev value
- * with current value to ensure that the search tree is proper
- * @queue: queue to add nodes to
- * @tree: binary search tree
- *
- * Return: 0 if is complete, 1 or greater if not
- */
-int bst(bt_queue_t **queue, const binary_tree_t *tree)
-{
-	int check = 0;
+	size_t r, l, height = 0;
 
 	if (tree)
 	{
-		check += bst(queue, tree->left);
-		check += bst_push_queue(queue, tree);
-		check += bst(queue, tree->right);
+		r = tree ? bt_height(tree->right) + 1 : 0;
+		l = tree ? bt_height(tree->left) + 1 : 0;
+		height += (r > l ? r : l);
 	}
-	return (check);
+	return (height);
 }
 /**
- * binary_tree_is_bst - checks if binary tree is proper balanced
- * AVL binary search tree
+ * _abs - returns the absolute value of the input number
+ * @num: the input number
+ *
+ * Return: the absolute value of the input number
+ */
+int _abs(int num)
+{
+	if (num < 0)
+		num *= -1;
+	return (num);
+}
+/**
+ * bt_balance - returns balance factor of every node of binary tree
+ * that is, the height of tree->left - height of tree->right
+ * @node: root of binary tree to find the balance factor
+ *
+ * Return: height of binary tree in size_t type
+ */
+int bt_balance(const binary_tree_t *node)
+{
+	int balance = 0, bal;
+
+	if (node)
+	{
+		bal = bt_height(node->left) - bt_height(node->right);
+		balance += (bal < 0 ? bal * -1 : bal) > 1;
+		balance += bt_balance(node->left) + bt_balance(node->right);
+	}
+	return (bal);
+}
+/**
+ * bst - traverses binary tree with inorder functionality, checking previoius
+ * minimum and maximum values, comparing each one along the way
+ * @node: the node to be compared
+ * @min: the minimum value to be checked with current node
+ * @max: the maximum value to be checked with current node
+ *
+ * Return: 1 if node is valid binary search tree node, 0 if not
+ */
+int bst(const binary_tree_t *node, const binary_tree_t *min,
+	const binary_tree_t *max)
+{
+	int valid;
+
+	if (node)
+	{
+		if ((min && min->n >= node->n) ||
+		    (max && max->n <= node->n))
+			valid = 0;
+		else
+			valid = (bst(node->left, min, node) &&
+				 bst(node->right, node, max));
+	}
+	else
+		valid = 1;
+	return (valid);
+}
+/**
+ * binary_tree_is_avl - checks if binary tree is proper/ valid avl binary
+ * search tree, balance factor is considered
  * @tree: input tree to check validity
  *
  * Return: 1 if is proper bst, 0 if false or failure
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	bt_queue_t *queue = NULL;
-	int check;
-
-	check = bst(&queue, tree);
-	bst_pop_queue(&queue);
-	return (tree && !check);
+	return (tree && bst(tree, NULL, NULL) && !bt_balance(tree));
 }
--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
 /**
- * bthb - returns height of a binary tree + 1, so leaves have height 1, and
+ * bt_height - returns height of a binary tree + 1, so leaves have height 1, &
  * NULL value is distinguishable from root leaf node
  * @tree: root of binary tree to find the height of
  *
  * Return: height of binary tree in size_t type + 1
  */
-size_t bthb(const binary_tree_t *tree)
+size_t bt_height(const binary_tree_t *tree)
 {
 	size_t r, l, height = 0;
 
 	if (tree)
 	{
-		r = tree ? bthb(tree->right) + 1 : 0;
-		l = tree ? bthb(tree->left) + 1 : 0;
+		r = tree ? bt_height(tree->right) + 1 : 0;
+		l = tree ? bt_height(tree->left) + 1 : 0;
 		height += (r > l ? r : l);
 	}
 	return (height);
@@ -27,14 +27,7 @@ size_t bthb(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance = 0;
-	size_t r, l;
-
 	if (tree)
-	{
-		l = bthb(tree->left);
-		r = bthb(tree->right);
-		balance = l - r;
-	}
-	return (balance);
+		return (bt_height(tree->left) - bt_height(tree->right));
+	return (0);
 }
