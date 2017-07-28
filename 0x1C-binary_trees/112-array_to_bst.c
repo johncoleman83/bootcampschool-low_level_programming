94631e0000f345196abe50a075a2da49c556b3c3
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-28 08:36:31 -0700

    builds a binary search tree, unbalanced based off of the input array

--- 0x1C-binary_trees/112-array_to_bst.c
+++ 0x1C-binary_trees/112-array_to_bst.c
@@ -1,5 +1,17 @@
 #include "binary_trees.h"
 /**
+ * array_to_bst - builds a binary tree based upon input array
+ * @array: the input array with integers
+ * @size: size of input array
  *
+ * Return: returns a binary search tree, unbalanced
  */
-bst_t *array_to_bst(int *array, size_t size);
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *tree = NULL;
+	size_t i;
+
+	for (i = 0; array && i < size; i++)
+		bst_insert(&tree, array[i]);
+	return (tree);
+}
