737867f2f7b0d56c0090340ee49da45f1898a617
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-28 08:52:40 -0700

    binary tree search, returns node in tree containing input value

--- 0x1C-binary_trees/113-bst_search.c
+++ 0x1C-binary_trees/113-bst_search.c
@@ -1,5 +1,19 @@
 #include "binary_trees.h"
 /**
+ * bst_search - searches for a value in a binary search tree
+ * @tree: the input tree to search for values in
+ * @value: the value to search for
  *
+ * Return: pointer to the discovered node or NULL
  */
-bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	while (tree && tree->n != value)
+	{
+		if (value > tree->n)
+			tree = tree->right;
+		else
+			tree = tree->left;
+	}
+	return ((bst_t *)tree);
+}
