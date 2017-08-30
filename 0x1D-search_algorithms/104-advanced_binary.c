1f6ef20ea8757b34b66507b9a6a28551bfc6874b
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-29 22:07:47 -0700

    updated binary search to return accurate index value

--- 0x1D-search_algorithms/104-advanced_binary.c
+++ 0x1D-search_algorithms/104-advanced_binary.c
@@ -1,5 +1,62 @@
 #include "search_algos.h"
+
 /**
- *
+ * print_array - prints the input array
+ * @array: the input array
+ * @size: the size of array
  */
-int advanced_binary(int *array, size_t size, int value);
+void print_array(int *array, size_t size)
+{
+	size_t i = 0;
+
+	printf("Searching in array: ");
+	while (i < size)
+	{
+		printf("%d", array[i]);
+		printf("%s", i++ < size - 1 ? ", " : "\n");
+	}
+}
+/**
+ * find_size - finds the new size of array
+ * @size: the size of array
+ * @mid: the middle that was checked
+ * @val_is_greater: 1 or TRUE if val is greater than middle integer
+ *
+ * Return: the new size
+ */
+size_t find_size(size_t size, size_t mid, int val_is_greater)
+{
+	if (size <= 1)
+		return (0);
+	if (val_is_greater && size % 2 != 0)
+		return (mid);
+	return (mid + 1);
+}
+/**
+ * advanced_binary - searches input array for input value using 'linear search'
+ * @array: the input array
+ * @size: the size of array
+ * @value: the value to search for
+ *
+ * Return: the index of where the found value is or -1 on error
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t mid;
+	int val_is_greater;
+
+	if (!array || !size)
+		return (-1);
+
+	if (value == array[0])
+		return 
+	print_array(array, size);
+	mid = (size - 1) / 2;
+	if (value == array[mid])
+		return (mid);
+
+	val_is_greater = value > array[mid];
+	size = find_size(size, mid, val_is_greater);
+	array += val_is_greater ? mid + 1 : 0;
+	return (advanced_binary(array, size, value));
+}
--- 0x1D-search_algorithms/main/1-main.c
+++ 0x1D-search_algorithms/main/1-main.c
@@ -14,10 +14,21 @@ int main(void)
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
+    printf("Found %d at index: %d\n\n", 0, binary_search(array, size, 0));
+    printf("Found %d at index: %d\n\n", 1, binary_search(array, size, 1));
     printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
-    printf("Found %d at index: %d\n\n", -5, binary_search(array, size, -5));
-    printf("Found %d at index: %d\n\n", 5, binary_search(array, 4, 5));
-    printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
+    printf("Found %d at index: %d\n\n", 3, binary_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 4, binary_search(array, size, 4));
+    printf("Found %d at index: %d\n\n", 5, binary_search(array, size, 5));
+    printf("Found %d at index: %d\n\n", 6, binary_search(array, size, 6));
+    printf("Found %d at index: %d\n\n", 7, binary_search(array, size, 7));
+    printf("Found %d at index: %d\n\n", 8, binary_search(array, size, 8));
+    printf("Found %d at index: %d\n\n", 9, binary_search(array, size, 9));
+    printf("size 2 array: Found %d at index: %d\n\n", 0, binary_search(array, 2, 0));
+    printf("size 2 array: Found %d at index: %d\n\n", 1, binary_search(array, 2, 1));
+    printf("size 1 array: Found %d at index: %d\n\n", 0, binary_search(array, 1, 0));
+    printf("out of range (less than): Found %d at index: %d\n\n", -5, binary_search(array, size, -5));
+    printf("out of range (greater than): Found %d at index: %d\n", 999, binary_search(array, size, 999));
     printf("Check NULL: %p %d\n", NULL, binary_search(NULL, size, 999));
     printf("Check size of %d %d\n", 0, binary_search(array, 0, 999));
     /*printf("Check large size %d %d\n", 25, binary_search(array, 25, 2));*/
