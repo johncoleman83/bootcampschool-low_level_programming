b93ee805973eb3d122b861539ee2649a6ffdd2d3
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-30 14:08:13 -0700

    updated variable names

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -1,21 +1,22 @@
 #include "search_algos.h"
 /**
  * linear_search - searches input array for input value using 'linear search'
- * @arr: the input array
+ * @array: the input array
  * @size: the size of array
  * @value: the value to search for
  *
  * Return: the index of where the found value is or -1 on error
  */
-int linear_search(int *arr, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (arr)
+	if (array)
 		while (i < size)
 		{
-			printf("Value checked array[%lu] = [%d]\n", i, arr[i]);
-			if (arr[i] == value)
+			printf("Value checked array[%lu] = [%d]\n", i,
+			       array[i]);
+			if (array[i] == value)
 				return (i);
 			i++;
 		}
