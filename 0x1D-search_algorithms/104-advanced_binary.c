9539946c6476e5e07699b64710964419e283af9e
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-29 22:17:28 -0700

    beginning template for advanced binary search

--- 0x1D-search_algorithms/104-advanced_binary.c
+++ 0x1D-search_algorithms/104-advanced_binary.c
@@ -17,20 +17,64 @@ void print_array(int *array, size_t size)
 	}
 }
 /**
- * find_size - finds the new size of array
+ * new_size - finds the new size of array
  * @size: the size of array
  * @mid: the middle that was checked
  * @val_is_greater: 1 or TRUE if val is greater than middle integer
  *
  * Return: the new size
  */
-size_t find_size(size_t size, size_t mid, int val_is_greater)
+size_t new_size(size_t size, size_t mid, int val_is_greater)
 {
 	if (size <= 1)
 		return (0);
 	if (val_is_greater && size % 2 != 0)
 		return (mid);
 	return (mid + 1);
+}
+/**
+ * new_idx - finds the new index to return of array
+ * @size: the size of array
+ * @idx: the old index value
+ * @val_is_greater: 1 or TRUE if val is greater than middle integer
+ *
+ * Return: the new index value
+ */
+size_t new_idx(size_t size, size_t idx, int val_is_greater)
+{
+	if (val_is_greater)
+		idx = idx + (size) / 2 + (size == 2 ? 0 : 1);
+	else
+		idx = idx - (size) / 2;
+	return (idx);
+}
+/**
+ * _abs - searches input array recursively for input value using 'binary search'
+ * @array: the input array
+ * @size: the size of array
+ * @idx: the index to be checked
+ * @value: the value to search for
+ *
+ * Return: the index of where the found value is or -1 on error
+ */
+int _abs(int *array, size_t size, size_t idx, int value)
+{
+	size_t mid;
+	int val_is_greater;
+
+	if (!array || !size)
+		return (-1);
+
+	print_array(array, size);
+	mid = (size - 1) / 2;
+	if (value == array[0])
+		return (idx);
+
+	val_is_greater = value > array[mid];
+	size = new_size(size, mid, val_is_greater);
+	array += val_is_greater ? mid + 1 : 0;
+	idx = new_idx(size, idx, val_is_greater);
+	return (_abs(array, size, idx, value));
 }
 /**
  * advanced_binary - searches input array for input value using 'linear search'
@@ -42,21 +86,7 @@ size_t find_size(size_t size, size_t mid, int val_is_greater)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t mid;
-	int val_is_greater;
+	int idx = ((size - 1) / 2);
 
-	if (!array || !size)
-		return (-1);
-
-	if (value == array[0])
-		return 
-	print_array(array, size);
-	mid = (size - 1) / 2;
-	if (value == array[mid])
-		return (mid);
-
-	val_is_greater = value > array[mid];
-	size = find_size(size, mid, val_is_greater);
-	array += val_is_greater ? mid + 1 : 0;
-	return (advanced_binary(array, size, value));
+	return (_abs(array, size, idx, value));
 }
--- 0x1D-search_algorithms/main/104-main.c
+++ 0x1D-search_algorithms/main/104-main.c
@@ -10,12 +10,14 @@
 int main(void)
 {
     int array[] = {
-        0, 1, 2, 5, 5, 6, 6, 7, 8, 9
+	    0, 1, 2, 5, 5, 6, 6, 7, 8, 9, 10, 10, 10
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
     printf("Found %d at index: %d\n\n", 8, advanced_binary(array, size, 8));
     printf("Found %d at index: %d\n\n", 5, advanced_binary(array, size, 5));
+    printf("Found %d at index: %d\n\n", 6, advanced_binary(array, size, 6));
+    printf("Found %d at index: %d\n\n", 10, advanced_binary(array, size, 10));
     printf("Found %d at index: %d\n", 999, advanced_binary(array, size, 999));
     return (EXIT_SUCCESS);
 }
