10267608742ba2f9877bec49f3177c57c00b0d07
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-01 14:58:15 -0700

    completed jump search algorithm

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -1,27 +1,24 @@
 #include "search_algos.h"
 #include <math.h>
 /**
- * linear_search - searches input array for input value using 'linear search'
+ * _ls - searches input array for input value using 'linear search'
  * @array: the input array
- * @size: the size of array
+ * @min: the minimum index value to check
+ * @max: the maximum index value to check
  * @value: the value to search for
  *
  * Return: the index of where the found value is or -1 on error
  */
-int linear_search(int *array, size_t size, int value)
+int _ls(int *array, size_t min, size_t max, int value)
 {
-	size_t i = 0;
-
-	if (array)
-		while (i < size)
-		{
-			printf("Value checked array[%lu] = [%d]\n", i,
-			       array[i]);
-			if (array[i] == value)
-				return (i);
-			i++;
-		}
-
+	while (min <= max)
+	{
+		printf("Value checked array[%lu] = [%d]\n", min,
+		       array[min]);
+		if (array[min] == value)
+			return (min);
+		min++;
+	}
 	return (-1);
 }
 /**
@@ -34,32 +31,32 @@ int linear_search(int *array, size_t size, int value)
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, check, prev, range;
+	size_t i, check, prev, step, range;
 
 	range = (size_t)sqrt(size);
 
 	if (array && size)
 	{
-		prev = 0;
+		step = prev = 0;
 		while (1 == 1)
 		{
-			check = prev + range;
+			check = step * range;
 			if (check >= size)
 				i = size - 1;
 			else
 				i = check;
 
-			if (array[i] > value || check >= size)
+			if (array[i] >= value || check >= size)
 			{
 				printf("Value found between indexes");
 				printf(" [%lu] and [%lu]\n", prev, i);
-				return (linear_search(array + prev, i - prev,
-						      value));
+				return (_ls(array, prev, i, value));
 			}
 
 			printf("Value checked array[%lu] = [%d]\n",
 			       i, array[i]);
 			prev = i;
+			step++;
 		}
 	}
 	else
